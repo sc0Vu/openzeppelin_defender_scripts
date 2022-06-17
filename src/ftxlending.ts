745867a2355e63b2376e57728ce5bbcc7f74bba5
sc0Vu <alk03073135@gmail.com>
2022-06-17 13:04:43 +0800

    ftx lending: fetch spot marging lending rate from api

--- src/ftxlending.ts
+++ src/ftxlending.ts
@@ -73,26 +73,55 @@ export async function handler({ secrets }: { secrets: SecretInfo }) {
     throw new Error('failed to post spot margin offers')
   }
 
+  const getSpotMarginLendingRates = async () : Promise<Record<string,any>> => {
+    const path = '/api/spot_margin/lending_rates'
+    const method = 'GET'
+    const res = await sendRequest(method, path)
+    if (res.status == 200) {
+      const data = res.data
+      const spotMarginLendingRates: Record<string,any> = {}
+      if (data.success) {
+        for (let i=0; i<data.result.length; i++) {
+          const coin = data.result[i]
+          spotMarginLendingRates[coin.coin] = coin
+        }
+      }
+      return spotMarginLendingRates
+    }
+    throw new Error('failed to get spot margin lending rates')
+  }
+
   const res = await getWalletBalance()
   if (res.info && res.info.success) {
     // loop for all coins that balance > 0
-    const promises = res.info.result.filter((r: Record<string, any>) => {
+    const availBalance = res.info.result.filter((r: Record<string, any>) => {
       return (new BN(r.total)).gt(ZERO)
-    }).map((r: Record<string, any>) => {
+    })
+    const spotMarginLendingRates = await getSpotMarginLendingRates()
+    const promises = availBalance.map((r: Record<string, any>) => {
+      const lendingRate = spotMarginLendingRates[r.coin]
+      const rate = lendingRate && lendingRate.estimate > 0 ? lendingRate.estimate : 1e-6
+      console.log(`[ftx-lending] lend ${r.coin} ${rate}`)
       return postSpotMarginOffers({
         coin: r.coin,
         size: r.total,
-        rate: 1e-6
+        rate: rate
       })
     })
-    const reses: Array<Record<string, any>> = await Promise.all(promises)
-    for (let i = 0; i < reses.length; i++) {
-      if (reses[i].success) {
-        console.log(`[ftx-lending] lend ${res.info.result[i].coin} success`)
-      } else {
-        const message = `[ftx-lending] failed to lend ${JSON.stringify(reses[i])}`
-        await sendTGMsg(tgToken, chatID, message)
+    let reses: Array<Record<string, any>> = []
+    try {
+      reses = await Promise.all(promises)
+      for (let i = 0; i < reses.length; i++) {
+        if (reses[i].success) {
+          console.log(`[ftx-lending] lend ${res.info.result[i].coin} success`)
+        } else {
+          const message = `[ftx-lending] failed to lend ${res.info.result[i].coin} ${JSON.stringify(reses[i])}`
+          await sendTGMsg(tgToken, chatID, message)
+        }
       }
+    } catch (err) {
+      const message = `[ftx-lending] failed to lend ${err.response.data}`
+      await sendTGMsg(tgToken, chatID, message)
     }
     return reses
   } else {
