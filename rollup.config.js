45a9e34dd9a6aae9e0fda304fff2851a706c4886
sc0vu <alk03073135@gmail.com>
2023-04-19 16:37:20 +0800

    Upgrade rollup to v3

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,23 @@
-import resolve from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
 import builtins from 'builtin-modules';
 import terser from '@rollup/plugin-terser';
+// "node:" protocol
+const nodePrefixedBuiltins = builtins.concat(builtins.map(x => 'node:' + x));
 const baseConfig = {
   plugins: [
-    resolve({ preferBuiltins: true }),
+    nodeResolve({ preferBuiltins: true }),
     commonjs(),
     json({ compact: true }),
     typescript(),
   ],
   treeshake: true,
   external: [
-    ...builtins,
+    ...nodePrefixedBuiltins,
     'ethers',
     'axios',
-    'node:os',
-    'node:process',
-    'node:tty',
     /^defender-relay-client(\/.*)?$/
   ],
 }
